83d1f65544c664835f339aa7cffa570de76619c0
Ivga <borozdinaes89@gmail.com>
2023-03-01 18:56:19 +0500

    seminar 5 task 1

--- seminar_05/task1.py
+++ seminar_05/task1.py
@@ -3,3 +3,12 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
+def degreeRec(number, degree):
+    if degree == 0:
+        return 1
+    return number * degreeRec(number, degree - 1)
+
+number = int(input('Enter number: '))
+degree = int(input('Enter degree: '))
+
+print(degreeRec(number, degree))
